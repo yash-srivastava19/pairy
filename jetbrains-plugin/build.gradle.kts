7df8710809179721f927d6644138f0a602e5b494
yash-srivastava19 <ysrivastava82@gmail.com>
2026-07-21 23:02:20 +0530

    feat(jetbrains): scaffold Gradle plugin project

---
+++ jetbrains-plugin/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.serialization") version "2.3.21"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
+}
+
+group = "dev.yashsrivastava19"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        local("/home/yashs/pycharm-2026.1.4")
+    }
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set("241")
+            untilBuild.set(provider { null })
+        }
+    }
+}
